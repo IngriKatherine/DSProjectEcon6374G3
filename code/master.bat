813d5040de9d6ec99b7cf1866f81d774961318c7
IngriKatherine <59847854+IngriKatherine@users.noreply.github.com>
2025-10-28 10:07:58 -0400

    Added one version of the proc data
    It is still too large and complicated, I will simplify it today

--- code/master.bat
+++ code/master.bat
@@ -1,4 +1,5 @@
 rem Batch File to execute the Filtering
 
-rem test
-R CMD BATCH "%~dp001_readfilterdata.R"
+rem R CMD BATCH "%~dp001_readfilterdata.R"
+
+R CMD BATCH "C:\Users\k_the\main\01_prob_stat\DataScienceProjectEcon6374Group3\code\test.R"
